31e93e3fdb3b4679ebf872d269ac6cdcfd4991a6
= <=>
2022-11-30 19:22:07 +0100

    added more words to data

--- src/scala/main/main.scala
+++ src/scala/main/main.scala
@@ -2,7 +2,7 @@ package Ciphers
 
 @main def main(): Unit = {
     import scala.io.StdIn.readLine
-    val data = Vector("hello", "lol", "hey", "the", "cat", "more", "i", "can", "would", "you", "me", "because", "he", "she", "how", "do", "please")
+    val data = Vector("hello", "lol", "hey", "the", "cat", "more", "i", "can", "would", "you", "me", "because", "he", "she", "how", "do", "please", "where", "so", "that")
     val cipher = CeasarCipher(readLine, data)
     val krypterat = cipher.encryptRandom()
     println(krypterat)
