1914b6fab379c92027c782dd9a99baaadbd5f59f
= <=>
2022-12-11 00:36:03 +0100

    added nice toString

--- src/main/scala/VigenereCipher.scala
+++ src/main/scala/VigenereCipher.scala
@@ -3,17 +3,30 @@ package ciphers
 class VigenereCipher() {
 
     def generateTable() =
+        var first = true
         import scala.collection.mutable.Buffer
         var alphabetRow = Letter.alphabet.toBuffer
 
         def shiftAlphabet(): Buffer[Char] =
-            var tempLetter = alphabetRow.last
-            alphabetRow -= tempLetter
-            alphabetRow.prepend(tempLetter)
-            alphabetRow
+            if !first then
+                var tempLetter = alphabetRow.last
+                alphabetRow -= tempLetter
+                alphabetRow.prepend(tempLetter)
+                alphabetRow
+            else 
+                first = false
+                alphabetRow
 
         var table: Vector[Vector[Char]] = Vector.fill(alphabetRow.size)(shiftAlphabet().toVector)    
         table
         
-    lazy val VigenereTable: Vector[Vector[Char]] = generateTable()
+    lazy val vigenereTable: Vector[Vector[Char]] = generateTable()
+
+    override def toString(): String =
+        var result = ""
+        vigenereTable.foreach(row => 
+            result += row.mkString.replace("", " ").trim() 
+            result += "\n"
+        )
+        result
 }
